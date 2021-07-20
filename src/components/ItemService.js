98760ba1463db9230c80abc4be9e210bf295ea14
Joel <cdiaz_jl@hotmail.com.ar>
2021-07-19 20:34:01 -0500

    Paypal button

--- src/components/ItemService.js
+++ src/components/ItemService.js
@@ -1,7 +1,40 @@
 import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom"
 import { Button, Card, Col, Descriptions, Modal, Row } from "antd";
 
+const PayPalButton = window.paypal.Buttons.driver("react", { React, ReactDOM });
+
 const ItemService = ({ services }) => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const createOrder = (data, actions) => {
+    return actions.order.create({
+      purchase_units: [
+        {
+          amount: {
+            value: "0.01",
+          },
+        },
+      ],
+    });
+  }
+
+  const onApprove = (data, actions) => {
+    return actions.order.capture();
+  }
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
   return (
     <Row style={{ margin: "0 10%" }}>
       {services.map((service) => {
@@ -11,11 +44,8 @@ const ItemService = ({ services }) => {
               style={{ width: 200 }}
               cover={<img alt="Not Found Image" src={service.photoURL} />}
               actions={[
-                <Button
-                  type="link"
-                  // onClick={() => handleViewMore(movie.imdbID)}
-                >
-                  Ver más
+                <Button type="primary" onClick={showModal}>
+                  Open Modal
                 </Button>,
               ]}
             >
@@ -27,6 +57,15 @@ const ItemService = ({ services }) => {
           </Col>
         );
       })}
+      <Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <PayPalButton
+            createOrder={(data, actions) => createOrder(data, actions)}
+            onApprove={(data, actions) => onApprove(data, actions)}
+        />
+      </Modal>
     </Row>
   );
 };
